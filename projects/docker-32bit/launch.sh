b8d8d9d25801d889865f03b558d0cc803efc6adb
ati-ince <atahir.ince@gmail.com>
2020-03-14 20:02:07 +0000

    update gf scrips

---
+++ projects/docker-32bit/launch.sh
@@ -0,0 +1,5 @@
+# docker run --rm --net=host -v $HOME/Projects:$HOME/Projects --name "NT" -it nt
+# docker run -i  i386/ubuntu:14.04
+# docker exec -it 5be5 /bin/bash
+
+#docker run -it --mount type=bind,source=/home/yazilim/Projects,target=/home/Projects 830bcd8ad3a2
