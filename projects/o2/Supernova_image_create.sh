d405c802a34afe1b654e61cbee78b0c2f3c2676f
ati-ince <atahir.ince@gmail.com>
2020-03-16 08:54:55 +0000

    update o2

--- projects/o2/Supernova_image_create.sh
+++ projects/o2/Supernova_image_create.sh
@@ -1,11 +1,11 @@
-cd trunk/source/Supernova/projects/
-source source board/macan/buildsettings/build_Macan_253A_ROM_EMMC_DVB.sh
+#cd trunk/source/Supernova/projects/
+#source board/macan/buildsettings/build_Macan_253A_ROM_EMMC_DVB.sh
 
-make image_all 
+#make image_all 
 
-#cd trunk/source/Supernova/target
-#./build_sw_upgrade_image.sh
+cd trunk/source/Supernova/target
+./build_sw_upgrade_image.sh
 
 
 #and carry for usb
-#cp europe_dtv.munich/images/ubifs/G7_SWImage.bin ../../../Target_G8/image/
+cp europe_dtv.macan/images/ext4/O2_SWImage.bin ../../../Target_O2/image/
