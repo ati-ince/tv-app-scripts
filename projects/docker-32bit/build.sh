d7e539a5f8be31ecd2b82bd4e1d6580abec7b2ce
ati-ince <atahir.ince@gmail.com>
2020-03-18 23:27:50 +0000

    update dockerfiles

--- projects/docker-32bit/build.sh
+++ projects/docker-32bit/build.sh
@@ -1,1 +1,1 @@
-sudo docker build -t artv32bits:work12 .
+sudo docker build -t artv32bits:work13 .
--- projects/docker-32bit/launch.sh
+++ projects/docker-32bit/launch.sh
@@ -2,5 +2,6 @@
 # docker run -i  i386/ubuntu:14.04
 # docker exec -it 5be5 /bin/bash
 
-#docker run -it --mount type=bind,source=/home/yazilim/Projects,target=/home/Projects 5725f3595e30 /bin/bash
+#docker run -it --mount type=bind,source=/home/yazilim/Projects,target=/home/Projects 1f5274ebca35 /bin/bash  -> 77ed294adffa   sleepy_buck  v12 güzel seviyede 
 
+#docker run -it --mount type=bind,source=/home/yazilim/Projects,target=/home/Projects 186dbf39efdf /bin/bash  -> b5b3a4387e06 vibrant_matsumoto  v13 yeni 
