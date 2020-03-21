ea2ff214b4a5ba1ab862a1778330341eddaf701d
ati-ince <atahir.ince@gmail.com>
2020-03-21 14:00:15 +0000

    add docker32bit 16.04 version

---
+++ projects/docker-32bit-16.04/build.sh
@@ -0,0 +1,1 @@
+sudo docker build -t artv32bits1604:work1 .
---
+++ projects/docker-32bit-16.04/launch.sh
@@ -0,0 +1,12 @@
+# docker run --rm --net=host -v $HOME/Projects:$HOME/Projects --name "NT" -it nt
+# docker run -i  i386/ubuntu:14.04
+# docker exec -it 5be5 /bin/bash
+
+#docker run -it --mount type=bind,source=/home/yazilim/Projects,target=/home/Projects 1f5274ebca35 /bin/bash  -> 77ed294adffa   sleepy_buck  v12 güzel seviyede 
+
+#docker run -it --mount type=bind,source=/home/yazilim/Projects,target=/home/Projects 186dbf39efdf /bin/bash  -> b5b3a4387e06 vibrant_matsumoto  v13 yeni 
+
+
+
+
+#docker run -it --mount type=bind,source=/home/yazilim/Projects,target=/home/Projects 2564eab2d11d /bin/bash  ->   v14 yeni 
